40afa0e84cc8ce1c542cb411fb5693ba4c4ae545
User <paadams@nmu.edu>
2017-08-29 19:49:31 -0400

    updated Home component

---
+++ src/components/Home.vue
@@ -0,0 +1,41 @@
+<template>
+  <q-layout
+    ref="layout"
+    view="lHh Lpr fff"
+    :left-class="{'bg-grey-2': true}"
+  >
+    <q-toolbar slot="header" class="glossy">
+      <q-btn
+        flat
+      >
+        <q-icon name="menu" />
+      </q-btn>
+      <q-toolbar-title>
+       Fishigan
+        <div slot="subtitle">Providing comprehensive information about fishing in Michigan </div>
+      </q-toolbar-title>
+    </q-toolbar>
+  </q-layout>
+</template>
+
+<script>
+import {
+  QToolbar,
+  QToolbarTitle,
+  QIcon,
+  QLayout
+} from 'quasar'
+export default {
+  name: 'home',
+  components:{
+    QToolbar,
+    QToolbarTitle,
+    QIcon,
+    QLayout
+  }
+}
+</script>
+
+<style lang="stylus">
+
+</style>

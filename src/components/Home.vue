8b1eade1423adf17a679f24a870d87f99965a96d
User <paadams@nmu.edu>
2017-08-30 08:36:36 -0400

    added list of native fish

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -15,22 +15,46 @@
         <div slot="subtitle">Providing comprehensive information about fishing in Michigan </div>
       </q-toolbar-title>
     </q-toolbar>
+    <h5>Fish native to Michigan</h5>
+    <ul>
+      <li v-for="(f, key) in fish" :key="key">
+        <p>Name: {{f.commonname}} {{f.latinname === undefined ? ' ' : `(${f.latinname})`}}</p>
+        <p>{{f.narrative}}</p>
+      </li>
+    </ul>
   </q-layout>
 </template>
 
 <script>
-import {
-  QToolbar,
-  QToolbarTitle,
-  QIcon,
-  QLayout
-} from 'quasar'
+import {QToolbar, QToolbarTitle, QIcon, QBtn, QLayout} from 'quasar'
+
 export default {
   name: 'home',
-  components:{
+  created () {
+    this.loadFishData()
+  },
+  data () {
+    return {
+      fish: []
+    }
+  },
+  methods: {
+    loadFishData () {
+      this.$http.get('https://data.michigan.gov/resource/gedb-8ff3.json')
+        .then(res => {
+          this.fish = res.data
+          console.log(res)
+        })
+        .catch(err => {
+          console.log(err)
+        })
+    }
+  },
+  components: {
     QToolbar,
     QToolbarTitle,
     QIcon,
+    QBtn,
     QLayout
   }
 }
